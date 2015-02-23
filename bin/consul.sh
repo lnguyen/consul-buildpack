9fc161c7fc8d4792a6870a7e32be63952f7129bc
Long Nguyen <long.nguyen11288@gmail.com>
2015-02-23 17:55:56 -0500

    fixed binding port

--- bin/consul.sh
+++ bin/consul.sh
@@ -2,4 +2,5 @@
 
 $HOME/.consul/consul agent -data-dir $HOME/.consul/data \
    -client $CF_INSTANCE_IP -join 10.10.8.6 \
-  > $HOME/.consul/log/consol.log &
+   -bind=0.0.0.0 \
+  > $HOME/.consul/log/consul.log &
