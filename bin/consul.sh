0b2e685bf702e199210a3d9caa25064a25623afa
Long Nguyen <long.nguyen11288@gmail.com>
2015-02-23 17:53:43 -0500

    fixed bin

--- bin/consul.sh
+++ bin/consul.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 $HOME/.consul/consul agent -data-dir $HOME/.consul/data \
    -client $CF_INSTANCE_IP -join 10.10.8.6 \
-  > $HOME/.consul/log/consol.log 2>&1
+  > $HOME/.consul/log/consol.log &
