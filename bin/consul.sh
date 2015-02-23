81bd1e6776fc065d8166a9f01d47c2ebe67553e5
Long Nguyen <long.nguyen11288@gmail.com>
2015-02-23 18:00:24 -0500

    changed ips

--- bin/consul.sh
+++ bin/consul.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 $HOME/.consul/consul agent -data-dir $HOME/.consul/data \
-   -client $CF_INSTANCE_IP -join 10.10.8.6 \
-   -bind=0.0.0.0 \
+   -client 0.0.0.0 -join 10.10.8.6 \
+   -bind=0.0.0.0 -advertise $CF_INSTANCE_IP\
   > $HOME/.consul/log/consul.log &
