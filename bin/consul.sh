94a5616280c3f08f9ab72014e3854f7d7b1c36a0
Long Nguyen <long.nguyen11288@gmail.com>
2015-02-23 22:36:08 -0500

    added consul config

--- bin/consul.sh
+++ bin/consul.sh
@@ -1,6 +1,42 @@
 #!/bin/sh
 
-$HOME/.consul/consul agent -data-dir $HOME/.consul/data \
-   -client 0.0.0.0 -join 10.10.8.6 \
-   -bind=0.0.0.0 -advertise $CF_INSTANCE_IP\
+jq=$HOME/.consul/jq
+
+server_rpc=$(echo $VCAP_APPLICATION | $jq .extra_ports.server_rpc.host_port)
+serf_lan=$(echo $VCAP_APPLICATION | $jq .extra_ports.serf_lan.host_port)
+serf_wan=$(echo $VCAP_APPLICATION | $jq .extra_ports.serf_wan.host_port)
+cli_rpc=$(echo $VCAP_APPLICATION | $jq .extra_ports.cli_rpc.host_port)
+http_api=$(echo $VCAP_APPLICATION | $jq .extra_ports.http_api.host_port)
+dns_interface=$(echo $VCAP_APPLICATION | $jq .extra_ports.dns_interface.host_port)
+node_name=$(echo $VCAP_APPLICATION | $jq .application_name)-$CF_INSTANCE_INDEX
+
+cat <<EOF > $HOME/.consul/agent.json
+{
+  "data_dir": "$HOME/.consul/data",
+  "node_name": "${node_name}",
+  "bind_addr": "0.0.0.0",
+  "client_addr": "0.0.0.0",
+  "advertise_addr": "${CF_INSTANCE_IP}",
+  "domain": "consul",
+  "leave_on_terminate": false,
+  "log_level": "INFO",
+  "server": false,
+  "rejoin_after_leave": true,
+  "start_join": [
+    "10.10.8.6",
+    "10.10.8.7",
+    "10.10.8.8"
+  ],
+  "ports": {
+    "dns": "${dns_interface}",
+    "http": "${http_api}",
+    "rpc": "${cli_rpc}",
+    "serf_lan": "${serf_lan}",
+    "serf_wan": "${serf_wan}",
+    "server": "${server_rpc}"
+  }
+}
+EOF
+
+$HOME/.consul/consul agent -data-dir -config-file $HOME/.consul/agent.json \
   > $HOME/.consul/log/consul.log &
